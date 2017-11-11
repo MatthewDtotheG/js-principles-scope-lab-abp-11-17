36d7e53d182d945fdfa208f7caeba9b7b487b99f
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-11 01:43:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-// Write your solution in this file!
-var customerName = 'bob';
-
-function upperCaseCustomerName() {
-  customerName.toUpperCase();
-
-  return upperCaseCustomerName()
-}
+var customerName = "bob"
